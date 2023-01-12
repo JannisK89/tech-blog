e099852bec725b4be385ba67c228791d663bf007
Jannis K <jannis.karanikis@gmail.com>
2023-01-12 17:04:51 +0100

    added dark mode and test toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import Header from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorMode, setColorMode] = useState('light')
+
+  const colorModeCheck = () => {
+    if (colorMode === 'light') {
+      setColorMode('dark')
+    } else setColorMode('light')
+  }
+  return (
+    <div className={colorMode}>
+      <div
+        className={
+          'w-full h-full bg-gray-100 dark:bg-gray-900 text-slate-900 dark:text-slate-100 '
+        }
+      >
+        <button onClick={colorModeCheck}>Test Mode</button>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
