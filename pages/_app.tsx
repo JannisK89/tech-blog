806d083b6c8546c5051da3e336dfa14f29b1025c
Jannis K <jannis.karanikis@gmail.com>
2023-01-13 10:39:20 +0100

    localstorage for darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [colorMode, setColorMode] = useState('light')
+  const [colorMode, setColorMode] = useState('')
 
-  const darkModeCheck= () => {
+  useEffect(() => {
+    setColorMode(window.localStorage.getItem('darkMode') ?? 'light')
+  }, [])
+
+  const darkModeCheck = () => {
     if (colorMode === 'light') {
       setColorMode('dark')
-    } else setColorMode('light')
+      window.localStorage.setItem('darkMode', 'dark')
+    } else {
+      setColorMode('light')
+      window.localStorage.setItem('darkMode', 'light')
+    }
   }
   return (
     <div className={colorMode}>
-      <div
-        className={
-          'w-full h-full bg-gray-100 dark:bg-gray-900 text-slate-900 dark:text-slate-100 '
-        }
-      >
-        <Header colorMode={colorMode} darkModeCheck={darkModeCheck}/>
+      <div className={'w-full h-full bg-gray-100 dark:bg-gray-900 text-slate-900 dark:text-slate-100 '}>
+        <Header colorMode={colorMode} darkModeCheck={darkModeCheck} />
         <Component {...pageProps} />
       </div>
     </div>
