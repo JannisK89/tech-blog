f807aa8cef8c128c8baec6ac497d3e992ceb3e20
Jannis K <jannis.karanikis@gmail.com>
2023-01-16 16:21:45 +0100

    fixed tag css for mobile

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -4,7 +4,7 @@ type Props = {
 }
 
 const Tag = ({ text, color }: Props) => {
-  return <span className={`${color} rounded p-2 text-black inline-block mr-4`}>{`#${text}`}</span>
+  return <span className={`${color} rounded p-2 text-black inline-block mr-4 my-2 md:my-0`}>{`#${text}`}</span>
 }
 
 export default Tag
