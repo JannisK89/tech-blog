aadc3192e7120fb7b38c067e952d6902df41da32
Jannis K <jannis.karanikis@gmail.com>
2023-01-16 18:03:56 +0100

    post page width fix

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -10,8 +10,8 @@ type Props = {
 
 const Post = ({ data, content }: Props) => {
   return (
-    <div className="max-w-4xl md:mx-auto  my-8  mx-6 h-full">
-      <article title={data.title} className="prose prose-slate dark:prose-invert ">
+    <div className=" md:mx-auto  my-8  mx-6 h-full">
+      <article title={data.title} className="prose prose-slate dark:prose-invert max-w-4xl ">
         <MDXRemote {...content} />
       </article>
     </div>
